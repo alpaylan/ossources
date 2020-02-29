9e1b256c35e0869b25ba00509988b790eee0e6ff
alpaylan <alpkeles99@gmail.com>
2020-03-01 01:07:53 +0300

    Commented for dummies.

--- ipc/pipe/pipe1.c
+++ ipc/pipe/pipe1.c
@@ -7,17 +7,17 @@ int main() {
 	int p;
 	int fd[2];
 
-	pipe(fd);
-	if (p=fork()) {
-		close(fd[0]);
-		dup2(fd[1],1);
-		close(fd[1]);
-		execl("/bin/ls","ls","-al",(char *)0);
-	} else {
-		close(fd[1]);
-		dup2(fd[0],0);
-		close(fd[1]);
-		execl("/usr/bin/tr","tr","/a-z/","/A-Z/",(char *)0);
+	pipe(fd);													// Pipe is opened with 1 file descriptor for read and 1 for write
+	if (p=fork()) {												// Parent process returns non-zero value
+		close(fd[0]);											// Close reading option for pipe
+		dup2(fd[1],1);											// Stdout is forwarded to pipe
+		close(fd[1]);											// Close writing option for pipe
+		execl("/bin/ls","/bin/ls","-al",(char *)0);				// Execute ls -al in the working directory with execl syscall
+	} else {													// Child process returns zero 
+		close(fd[1]);											// Close writing option for pipe
+		dup2(fd[0],0);											// Pipe read is forwarded to stdin
+		close(fd[1]);											// Close reading option for pipe
+		execl("/usr/bin/tr","tr","/a-z/","/A-Z/",(char *)0);	// stdin(pipe) input is converted uppercase and forwarded to stdout
 	}
 
 	return 0;
